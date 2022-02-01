e769da640e7a321fd318563b7b7ddba52587566f
QuantumA <a.cperezdev@gmail.com>
2022-02-01 17:59:41 +0100

    Some doc

--- src/allocation/domain/model.py
+++ src/allocation/domain/model.py
@@ -3,10 +3,21 @@ from dataclasses import dataclass
 from datetime import date
 from typing import Optional, List, Set
 from . import commands, events
+from typing import NewType
 
 
 class Product:
+    """
+    Representa un producto que puede ser comprado.
+    """
     def __init__(self, sku: str, batches: List[Batch], version_number: int = 0):
+        """
+
+        Args:
+            sku (str): Se utiliza para identificar un producto (stock-keeping unit)
+            batches (List[Batch]): [description]
+            version_number (int, optional): [description]. Defaults to 0.
+        """
         self.sku = sku
         self.batches = batches
         self.version_number = version_number
@@ -40,13 +51,38 @@ class Product:
 
 @dataclass(unsafe_hash=True)
 class OrderLine:
+    """Reprsenta los pedidos que pueden hacer los clientes.
+    Esta tiene un sku y una cantidad (qty)
+    """
     orderid: str
     sku: str
     qty: int
 
-
+Quantity = NewType("Quantity", int)
+Sku = NewType("Sku", str)
+Reference = NewType("Reference", str)
 class Batch:
-    def __init__(self, ref: str, sku: str, qty: int, eta: Optional[date]):
+    """El departamento de compras pide pequeños lotes de stock.
+       Un lote de stock tiene un ID único llamado referencia, una SKU y una cantidad.
+
+       Tenemos que asignar líneas de pedido a los lotes.
+       Cuando hayamos asignado una línea de pedido a un lote, enviaremos las existencias 
+       de ese lote específico a la dirección de entrega del cliente.
+       Cuando asignamos x unidades de stock a un lote, la cantidad disponible se reduce en x.
+
+       Ej:
+        - Tenemos un lote de 20 SMALL-TABLE, y asignamos una línea de pedido para 2 SMALL-TABLE
+        - El lote quedará con 18 SMALL-TABLE restantes.
+
+        No podemos asignar a un lote si la cantidad disponible es menor que la cantidad de la línea de pedido.
+        - Tenemos un lote de 1 CEBOLLA AZUL, y una línea de pedido de 2 CEBOLLA AZUL.
+        - No deberíamos poder asignar la línea al lote.
+
+    """
+
+
+
+    def __init__(self, ref: Reference, sku: Sku, qty: Quantity, eta: Optional[date]):
         self.reference = ref
         self.sku = sku
         self.eta = eta
